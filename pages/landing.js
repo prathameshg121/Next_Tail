266bf8d3b8635bcc42448f12495d2a32ce6a10f7
prathameshg121 <ghogarep2000@gmail.com>
2022-04-14 23:15:28 +0530

    right box added

--- pages/landing.js
+++ pages/landing.js
@@ -31,11 +31,58 @@ function Landing() {
         </div>
       </header>
       <div className="flex ...">
-      <div class="grid grid-rows-3 grid-flow-col gap-4 w-4/5 px-auto">
-        <div class="col-span-2 ...">02</div>
-        <div class="row-span-2 col-span-2 ...">03</div>
-        <div class="row-span-3 ...">01</div>
-      </div>
+        <div class="grid grid-rows-3 grid-flow-col gap-4 w-4/5 mx-auto py-4">
+          <div class="col-span-2 ... text-6xl break-normal">
+            Get most exciting
+            <br /> job from all around
+            <br /> the world here
+          </div>
+          <div class="row-span-2 col-span-2 ...">
+            Work with the largest network independent professionals and get
+            <br /> things done - from quick turnarounds to big transformations.
+          </div>
+          <div class="row-span-3 ... bg-gray-100 p-4">
+            <button class="inline-flex items-center bg-blue-700 text-white text-center border-0 focus:outline-none hover:bg-blue-500 px-5 py-3 rounded text-base mt-4 md:mt-0 w-full">
+              Polpular Job
+            </button>
+            <div>
+              <div class="h-full bg-white bg-opacity-75 px-2 py-2 my-2 rounded-lg overflow-hidden text-center relative">
+                <div class="text-lg text-left">Senior Website Developer</div>
+                <div class="text-md text-left text-gray-400">Discord Inc.</div>
+                <div class="grid grid-flow-col gap-1 mx-auto py-2">
+                  <div class="col-span-2 text-left">55 people applied</div>
+                  <div class="col-span-1">
+                    <span class="inline-block py-1 px-2 rounded bg-indigo-50 text-indigo-500 float-right">Opened</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="h-full bg-white bg-opacity-75 px-2 py-2 my-2 rounded-lg overflow-hidden text-center relative">
+                <div class="text-lg text-left">Senior Website Developer</div>
+                <div class="text-md text-left text-gray-400">Discord Inc.</div>
+                <div class="grid grid-flow-col gap-1 mx-auto py-2">
+                  <div class="col-span-2 text-left">55 people applied</div>
+                  <div class="col-span-1">
+                    <span class="inline-block py-1 px-2 rounded bg-indigo-50 text-indigo-500 float-right">Opened</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="h-full bg-white bg-opacity-75 px-2 py-2 my-2 rounded-lg overflow-hidden text-center relative">
+                <div class="text-lg text-left">Senior Website Developer</div>
+                <div class="text-md text-left text-gray-400">Discord Inc.</div>
+                <div class="grid grid-flow-col gap-1 mx-auto py-2">
+                  <div class="col-span-2 text-left">55 people applied</div>
+                  <div class="col-span-1">
+                    <span class="inline-block py-1 px-2 rounded bg-indigo-50 text-indigo-500 float-right">Opened</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
